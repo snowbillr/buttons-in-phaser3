26da0bb9c3acd1af11fec219a33f26fc8526d826
Bill Reed <billnreed@gmail.com>
2018-04-12 07:43:35 -0400

    add event listeners to text

--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -1,5 +1,11 @@
 export class SimpleScene extends Phaser.Scene {
   create() {
-    this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
+    const helloButton = this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
+    helloButton.setInteractive();
+
+    helloButton.on('pointerover', () => console.log('pointerover'));
+    helloButton.on('pointerdown', () => console.log('pointerdown'));
+    helloButton.on('pointerup', () => console.log('pointerup'));
+    helloButton.on('pointerout', () => console.log('pointerout'));
   }
 }
