03921f1b74f8e94858404b18cac7b1bb91befb18
Bill Reed <billnreed@gmail.com>
2018-06-27 17:53:08 -0400

    add increment and decrement buttons

--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -1,50 +1,30 @@
+import { TextButton } from '../game-objects/text-button';
+
 export class SimpleScene extends Phaser.Scene {
-  preload() {
-    this.load.image('button', 'assets/button.png');
-    this.load.image('button-over', 'assets/button-over.png');
-    this.load.image('button-down', 'assets/button-down.png');
-  }
-
   create() {
-    // const helloButton = this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
-    /*
-    const helloButton = this.add.sprite(200, 200, 'button');
-    helloButton.setInteractive();
-
-    helloButton.on('pointerover', () => helloButton.setTexture('button-over'));
-    helloButton.on('pointerdown', () => helloButton.setTexture('button-down'));
-    helloButton.on('pointerup', () => helloButton.setTexture('button-over'));
-    helloButton.on('pointerout', () => helloButton.setTexture('button'));
-    */
-    let clickCount = 0;
+    this.clickCount = 0;
     this.clickCountText = this.add.text(100, 200, '');
 
-    this.clickButton = this.add.text(100, 100, 'Click me!', { fill: '#0f0' })
-      .setInteractive()
-      .on('pointerover', () => this.enterButtonHoverState() )
-      .on('pointerout', () => this.exitButtonHoverState() )
-      .on('pointerdown', () => this.enterButtonActiveState() )
-      .on('pointerup', () => {
-        this.updateClickCountText(++clickCount);
-        this.enterButtonHoverState();
-    });
+    this.incrementButton = new TextButton(this, 100, 100, 'Increment Count', { fill: '#0f0 '}, () => this.incrementClickCount());
+    this.add.existing(this.incrementButton);
 
-    this.updateClickCountText(clickCount);
+    this.decrementButton = new TextButton(this, 100, 150, 'Decrement Count', { fill: '#0f0 '}, () => this.decrementClickCount());
+    this.add.existing(this.decrementButton);
+
+    this.updateClickCountText();
   }
 
-  updateClickCountText(clickCount) {
-    this.clickCountText.setText(`Button has been clicked ${clickCount} times.`);
+  incrementClickCount() {
+    this.clickCount += 1;
+    this.updateClickCountText();
   }
 
-  enterButtonHoverState() {
-    this.clickButton.setStyle({ fill: '#ff0 '});
+  decrementClickCount() {
+    this.clickCount -= 1;
+    this.updateClickCountText();
   }
 
-  exitButtonHoverState() {
-    this.clickButton.setStyle({ fill: '#0f0 '});
-  }
-
-  enterButtonActiveState() {
-    this.clickButton.setStyle({ fill: '#0ff' });
+  updateClickCountText() {
+    this.clickCountText.setText(`Button has been clicked ${this.clickCount} times.`);
   }
 }
